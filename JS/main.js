7ca499fa251f81ae14d084eacec82333141e2a9b
CamaradaYorch <jorge.mera.torres@gmail.com>
2022-06-25 23:01:41 -0500

    Ultima modificación

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,60 @@
 
 /* let search_input = document.getElementById("search_button")*/
 
+let containerPokemon = document.getElementById("container-pokemon");
+let currentPokemonI = 0;
+/* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
+
 let pokemons = JSON.parse(poke_file)["result"];
 /* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
 
-function search() {
-  let searchInput = document.getElementById("search_input");
-  let containerPokemon = document.getElementById("container-pokemon");
+function nextPokemon() {
+  if (currentPokemonI == pokemons.length) return;
+  currentPokemonI++;
+  displayPokemon(pokemons[currentPokemonI]);
+}
 /* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
 
-  let pokemon = searchPokemon(searchInput.value);
+function previousPokemon() {
+    if (currentPokemonI == 0) return;
+    currentPokemonI--;
+    displayPokemon(pokemons[currentPokemonI]);
+}
+/* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
+
+function displayPokemon(pokemon) {
   if ( pokemon ) {
-    containerPokemon.innerHTML = `<div></div>`;
     containerPokemon.innerHTML = `
-    <p style="color:white;">
-        Nombre: ${pokemon.name}<br>
-        No.: ${pokemon.number}<br>
-        Tipo: ${pokemon.type}<br>
-        Debilidades: ${pokemon.weakness}<br>
+    <p style="color:white;"><br>
+        <strong> # </strong> ${pokemon.number}<br>
+        <strong> Tipo: </strong> ${pokemon.type}<br>
         <img src="${pokemon.ThumbnailImage}"/>
+        <strong> Nombre: </strong> ${pokemon.name}<br>
+        <strong> Debilidades: </strong> ${pokemon.weakness}<br>
+        
     </p>`;
   }
 }
 /* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
 
+function search() {
+  let searchInput = document.getElementById("search_input");
+  
+/* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
+
+  let pokemon = searchPokemon(searchInput.value);
+  displayPokemon(pokemon);
+}
+/* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
+
 function searchPokemon(name){
   for (let i = 0; i < pokemons.length; i++){
-    console.log(pokemons[i]);
-    if(pokemons[i].name == name) {
+    let nombreMayuscula = pokemons[i].name;
+    console.log(nombreMayuscula.toLowerCase(), name.toLowerCase(), nombreMayuscula.toLowerCase() == name.toLowerCase());
+    if(nombreMayuscula.toLowerCase() == name.toLowerCase()) {
+      currentPokemonI = i;
       return pokemons[i];
     }
   }
 }
-
 /* ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ ⚈ */
